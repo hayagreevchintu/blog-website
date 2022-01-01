da63e61ed18637ff4dd9b1478cb49b5d0280c013
Hayagreev <hayagreev.chintu1@gmail.com>
2022-01-01 22:55:06 +0530

    displayed contents of posts in home.ejs

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {homeContent: homeStartingContent});
-  console.log(posts);
+  res.render("home", {homeContent: homeStartingContent, posts: posts});
 });
 
 app.get("/about", (req, res) => {
